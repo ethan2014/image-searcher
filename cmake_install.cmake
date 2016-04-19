e28e19f88519d19574e7d23ebc655687e9a530ed
ethan2014 <lapeed@miamioh.edu>
2016-04-19 19:33:36 -0400

    CMakeFiles directory and compile_commands.json files are now ignored by git

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -38,7 +38,7 @@ else()
   set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
 endif()
 
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/ethan/projects/image-searcher/${CMAKE_INSTALL_MANIFEST}"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
+file(WRITE "/home/ethan/projects/image-searcher/${CMAKE_INSTALL_MANIFEST}" "")
+foreach(file ${CMAKE_INSTALL_MANIFEST_FILES})
+  file(APPEND "/home/ethan/projects/image-searcher/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
+endforeach()
